6723e899e0c3add2d9f925e3ad80d12ae953c3d3
Eleazar Laguna <eleazarlagunam@gmail.com>
2024-05-11 13:39:36 -0600

    change: Contacto model structure

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['fullname'];
+
+    public function phones()
+    {
+        return $this->hasMany(ContactPhone::class);
+    }
+
+    public function addresses()
+    {
+        return $this->hasMany(ContactAddress::class);
+    }
+
+    public function emails()
+    {
+        return $this->hasMany(ContactEmail::class);
+    }
 }
