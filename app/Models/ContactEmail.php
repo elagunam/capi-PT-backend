2c75b243d57517f08dfd0bf4f7ea7d29500f854f
Eleazar Laguna <eleazarlagunam@gmail.com>
2024-05-11 13:44:37 -0600

    change: ContactEmail model structure

--- app/Models/ContactEmail.php
+++ app/Models/ContactEmail.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ContactEmail extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['contact_id', 'email'];
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
 }
