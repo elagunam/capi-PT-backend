075b438f6128d4d887f68e8ad326220db19c9554
Eleazar Laguna <eleazarlagunam@gmail.com>
2024-05-11 13:43:34 -0600

    change: ContactAdress model structure

--- app/Models/ContactAddress.php
+++ app/Models/ContactAddress.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ContactAddress extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['contact_id', 'address', 'city', 'country', 'zip'];
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
 }
