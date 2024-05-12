df4604085a1cde86c8586a78b27a2767a477513d
Eleazar Laguna <eleazarlagunam@gmail.com>
2024-05-11 20:53:40 -0600

    add: Phones CRUD APIS

--- app/Http/Controllers/ContactPhoneController.php
+++ app/Http/Controllers/ContactPhoneController.php
@@ -5,62 +5,110 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreContactPhoneRequest;
 use App\Http\Requests\UpdateContactPhoneRequest;
 use App\Models\ContactPhone;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContactPhoneController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    
+
+    private function getPhonesForContactById($contact_id){
+        $phones = ContactPhone::where('contact_id', $contact_id)->where('deleted', 0)->get();
+        if(sizeof($phones) < 1){
+            return [];
+        }
+        return $phones;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function getOneById(Request $request){
+        $id = $request->id;
+        $phones = ContactPhone::where('id', $id)->get();
+        if(sizeof($phones) < 1){
+            $response['status'] = false;
+            $response['message'] = 'No se encontró información.';
+            return response()->json($response);
+        }
+
+        $response['status'] = true;
+        $response['phone'] = $phones[0];
+        $response['message'] = 'Se encontró información';
+        return response()->json($response);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreContactPhoneRequest $request)
-    {
-        //
+    public function save(Request $request){
+        
+
+        if(!$request->filled(['contact_id', 'phone_number', 'type'])){
+            $response['status'] = false;
+            $response['message'] = 'Debe completar el formulario para guardar la informacíon';
+            return response()->json($response);
+        }
+
+        $contact_id = $request->contact_id;
+        $phone_number = $request->phone_number;
+        $type = $request->type;
+
+        
+        if($request->filled(['id'])){
+            $id = $request->id;
+            $phones = ContactPhone::where('id', $id)->where('contact_id', $contact_id)->get();
+            if(sizeof($phones) < 1){
+                $response['status'] = false;
+                $response['message'] = 'No se encontró información especificada';
+                return response()->json($response);
+            }
+            $contactPhone = $phones[0];
+        }else{
+            $contactPhone = new ContactPhone;
+            $contactPhone->contact_id = $contact_id;
+        }
+
+        $contactPhone->address = $phone_number;
+        $contactPhone->type = $type;
+
+        
+
+        if(!$contactPhone->save()){
+            $response['status'] = false;
+            $response['message'] = '¡Oops! Parece que algo salió mal';
+            return response()->json($response);
+        }
+
+        $contactPhones = $this->getPhonesForContactById($contactPhone->contact_id);
+        
+
+        $response['status'] = true;
+        $response['phones'] = $contactPhones;
+        $response['message'] = 'Información guardada';
+        return response()->json($response);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(ContactPhone $contactPhone)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ContactPhone $contactPhone)
-    {
-        //
-    }
+    public function delete(Request $request){
+        $id = $request->id;
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateContactPhoneRequest $request, ContactPhone $contactPhone)
-    {
-        //
-    }
+        
+        $phones = ContactPhone::where('id', $id)->get();
+        if(sizeof($phones) < 1){
+            $response['status'] = false;
+            $response['message'] = 'No se encontró información';
+            return response()->json($response);
+        }
+        $contactPhone = $phones[0];
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(ContactPhone $contactPhone)
-    {
-        //
+        $contactPhone->deleted = 1;
+        $contactPhone->deleted_at = DB::raw('now()');
+
+        if(!$contactPhone->save()){
+            $response['status'] = false;
+            $response['message'] = '¡Oops! Parece que algo salió mal';
+            return response()->json($response);
+        }
+        $contactPhones = $this->getPhonesForContactById($contactPhone->contact_id);
+        
+        $response['status'] = true;
+        $response['phones'] = $contactPhones;
+        $response['message'] = 'Dirección eliminada';
+        return response()->json($response);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContactAddressController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactPhoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ Route::prefix('contacts')->controller(ContactController::class)->group(function(
 });
 
 Route::prefix('address')->controller(ContactAddressController::class)->group(function(){
+    Route::get('{id}', 'getOneById');
+    Route::post('', 'save');
+    Route::delete('{id}', 'delete');
+});
+
+Route::prefix('phones')->controller(ContactPhoneController::class)->group(function(){
     Route::get('{id}', 'getOneById');
     Route::post('', 'save');
     Route::delete('{id}', 'delete');
